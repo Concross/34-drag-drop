c2e739f432bda70d2166e5d85ce1cff21e550098
Concross <cacrossley22@gmail.com>
2018-10-22 14:28:15 -0700

    add store, reducer, hello world for dashboard

---
+++ src/component/dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class DashboardContainer extends React.Component {
+  render() {
+    return (
+      <div className="dashboard-container">
+        <p>Hello Dashboard!</p>
+      </div>
+    );
+  }
+}
