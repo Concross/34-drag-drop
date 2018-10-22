6258fb1c12678056dea1599a2afdbd91c456157e
Concross <cacrossley22@gmail.com>
2018-10-22 15:23:59 -0700

    form added to category form component

--- src/component/category-form/index.js
+++ src/component/category-form/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
 export default class CategoryForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      budget: '',
+    };
+  }
+
   render() {
     return (
-      <div className="category-form">
-        <p>Hello CategoryForm!</p>
+      <div className="category-form-container">
+        <form id="category-form">
+          <fieldset>
+            <legend>Category Form</legend>
+            <label htmlFor="name">
+              Category:
+              <input type="text" name="name" value={this.state.name} />
+            </label>
+            <label htmlFor="budget">
+              Budget:
+              <input type="text" name="budget" value={this.state.budget} />
+            </label>
+            <button type="submit">Create/Update</button>
+          </fieldset>
+        </form>
       </div>
     );
   }
