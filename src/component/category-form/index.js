a2f54827b0a1e53f83cdd413496d13f90d6c2000
Concross <cacrossley22@gmail.com>
2018-10-22 14:30:38 -0700

    add static hello world for category form

---
+++ src/component/category-form/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class CategoryForm extends React.Component {
+  render() {
+    return (
+      <div className="category-form">
+        <p>Hello CategoryForm!</p>
+      </div>
+    );
+  }
+}
--- src/component/dashboard/index.js
+++ src/component/dashboard/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CategoryForm from '../category-form';
 
 export default class DashboardContainer extends React.Component {
   render() {
     return (
       <div className="dashboard-container">
         <p>Hello Dashboard!</p>
+        <CategoryForm />
       </div>
     );
   }
