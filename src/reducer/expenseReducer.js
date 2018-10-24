686d13adacefdd0259d1257da711d9985795551f
Concross <cacrossley22@gmail.com>
2018-10-23 20:55:12 -0700

    expense updated when category is deleted

--- src/reducer/expenseReducer.js
+++ src/reducer/expenseReducer.js
@@ -6,6 +6,10 @@ export default (state = initialState, action) => {
   switch (type) {
     case 'CATEGORY_CREATE': return { ...state, [payload.id]: [] };
 
+    case 'CATEGORY_DESTROY':
+      delete state[payload.id];
+      return { ...state };
+
     case 'EXPENSE_CREATE':
       return { ...state, [payload.categoryId]: [...state[payload.categoryId], payload] };
 
