4a845042d2c98f3f2602532c4cfe9fbbd201d4ca
Concross <cacrossley22@gmail.com>
2018-10-26 13:21:05 -0700

    expenseReducer tested for expenseCreate action

--- src/reducer/__tests__/expenseReducer.test.js
+++ src/reducer/__tests__/expenseReducer.test.js
@@ -21,4 +21,40 @@ describe('expenseReducer', () => {
     };
     expect(actual).toEqual(expected);
   });
+
+  test('should remove the expense from the state if CATEGORY_DESTROY is dispatched', () => {
+    const category = {
+      id: 1,
+    };
+
+    const state = {
+      1: [],
+    };
+
+    const action = { type: 'CATEGORY_DESTROY', payload: category };
+    const actual = expenseReducer(state, action);
+    const expected = {};
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should create an expense if action is EXPENSE_CREATE', () => {
+    const state = {
+      1: [],
+    };
+
+    const expense = {
+      title: 'title',
+      cost: 55,
+      categoryId: 1,
+    };
+
+    const action = { type: 'EXPENSE_CREATE', payload: expense };
+    const actual = expenseReducer(state, action);
+    const expected = {
+      1: [expense],
+    };
+
+    expect(actual).toEqual(expected);
+  });
 });
